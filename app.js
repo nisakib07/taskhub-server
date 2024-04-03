0ad5d76e0ce28b73ba727e2a9ba18e2bf864f13f
nisakib07 <websakib07@gmail.com>
2024-04-03 23:56:08 +0600

    Cors added

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://taskhub-client-three.vercel.app",
+    ],
   })
 );
 export default app;
